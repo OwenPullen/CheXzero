ee0eb233d33bb9d48976e1e72e7feeff8aa1a263
Owen Pullen <owenpullen@hotmail.co.uk>
2024-05-15 19:15:50 +0100

    adding in ECE code

--- tta.py
+++ tta.py
@@ -44,7 +44,7 @@ transforms = monai.transforms.compose.Compose([
     monai.transforms.RandAffined(keys='image', prob = 0.2, rotate_range=(0.5,0.5), shear_range=(0.25,0.25))
 ])
 transforms = monai.transforms.RandomOrder(transforms)
-verbose = True
+verbose = False
 y_pred_tta = run_softmax_eval(model, loader, cxr_labels, cxr_pair_template, transforms=transforms, verbose=verbose)
 if verbose: 
     print('images saved')
@@ -59,9 +59,30 @@ cxr_results_tta: pd.DataFrame = eval.evaluate(test_pred_tta, test_true_tta, cxr_
 print(cxr_results)
 print(cxr_results_tta)
 
-cxr_results.append(cxr_results_tta).to_csv('results/tta_results.csv')
+from torchmetrics.classification import BinaryCalibrationError
+import re
 
-import matplotlib.pyplot as plt
+bce = BinaryCalibrationError(norm = 'l1')
+
+class_list = []
+class_list_tta = []
+for i in range(14):
+    err_no_tta = bce(preds = torch.tensor(test_pred[:,i]), target = torch.tensor(test_true[:,i]))
+    err_tta = bce(preds = torch.tensor(test_pred_tta[:,i]), target = torch.tensor(test_true_tta[:,i]))
+    print(err_no_tta, err_tta)
+    class_list.append([ #cxr_labels[i],
+                        err_no_tta, err_tta])
+
+    # class_list_tta.append(err_tta)
+print(class_list)
+
+# # join class list to cxr results and cxr_results_tta
+# cxr_results = cxr_results.join(pd.DataFrame(class_list, columns = ['BCE_no_tta']))
+# cxr_results = cxr_results.join(pd.DataFrame(class_list_tta, columns = ['BCE_tta']))
+
+# print(cxr_results)
+# print(cxr_results_tta)
+
 
 cxr2 = cxr_results.stack().reset_index(level =1)
 cxr2.columns = ['Label', 'AUC_no_tta']
@@ -69,16 +90,67 @@ print(cxr2)
 cxr2_tta = cxr_results_tta.stack().reset_index(level =1)
 cxr2_tta.columns = ['Label', 'AUC_tta']
 gph = pd.merge(cxr2, cxr2_tta, on='Label')
-print(gph)
-gph.to_csv('results/tta_results.csv')
-index = np.arange(len(gph))
-bar_width = 0.35
-plt.bar(gph['Label'], gph['AUC_no_tta'], label = 'No TTA')
-plt.bar(gph['Label'], gph['AUC_tta'], label='TTA')
-plt.ylabel('AUC')
-plt.title('AUC of model with and without TTA')
-plt.xticks(rotation=90)
-plt.legend(loc='upper right', bbox_to_anchor=(1, 1.25))
-plt.tight_layout()
-plt.savefig('results/tta_results.png')
-print(gph)
+gph2 = pd.DataFrame.join(gph, pd.DataFrame(np.array(class_list), columns = ['BCE_no_tta', 'BCE_tta']))
+print(gph2)
+
+# Remove "_auc" from the Label column
+gph2['Label'] = gph2['Label'].apply(lambda x: re.sub('_auc', '', x))
+
+# Round all columns to 3 decimal places
+gph2 = gph2.round(3)
+
+# Save the modified dataframe to a CSV file
+gph2.to_csv('results/tta_results.csv', index=False)
+
+class_list = []
+for i in range(14):
+    err_no_tta = bce(preds = torch.tensor(test_true[:,i]).float(), target = torch.tensor(test_true[:,i]))
+    err_tta = bce(preds = torch.tensor(test_true[:,i]).float(), target = torch.tensor(test_true[:,i]))
+    print(err_no_tta, err_tta)
+    class_list.append([ #cxr_labels[i],
+                        err_no_tta, err_tta])
+
+    # class_list_tta.append(err_tta)
+print(class_list)
+
+# from torchmetrics.classification.calibration_error import MulticlassCalibrationError
+# from torchmetrics.wrappers import ClasswiseWrapper
+# calibration_error = ClasswiseWrapper(MulticlassCalibrationError(num_classes=14, norm='l1')) # L! norm makes it ECE
+# test_true = np.array(test_true); 
+#.to(torch.float16)#, 0)
+# test_true_one_hot = torch.eye(14)[test_true]
+# # ece = calibration_error(preds = test_pred, target = test_true.argmax(-1))
+# print('Calibration Error without TTA: ', ece)
+# ece_tta = calibration_error(torch.tensor(test_pred_tta), test_true_one_hot)
+#                             #  torch.tensor(test_true_tta).argmax(-1))
+# print('Calibration Error with TTA: ', ece_tta)
+
+
+
+
+
+# cxr_results.append(cxr_results_tta).to_csv('results/tta_results.csv')
+
+# import matplotlib.pyplot as plt
+
+# cxr2 = cxr_results.stack().reset_index(level =1)
+# cxr2.columns = ['Label', 'AUC_no_tta']
+# print(cxr2)
+# cxr2_tta = cxr_results_tta.stack().reset_index(level =1)
+# cxr2_tta.columns = ['Label', 'AUC_tta']
+# gph = pd.merge(cxr2, cxr2_tta, on='Label')
+# print(gph)
+# gph.to_csv('results/tta_results.csv')
+# index = np.arange(len(gph))
+# bar_width = 0.35
+# plt.bar(gph['Label'], gph['AUC_no_tta'], label = 'No TTA')
+# plt.bar(gph['Label'], gph['AUC_tta'], label='TTA')
+# plt.ylabel('AUC')
+# plt.title('AUC of model with and without TTA')
+# plt.xticks(rotation=90)
+# plt.legend(loc='upper right', bbox_to_anchor=(1, 1.25))
+# plt.tight_layout()
+# plt.savefig('results/tta_results.png')
+# print(gph)
+
+
